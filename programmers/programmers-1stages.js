41c24e076ddd4b73af2e06b127cbf650c1f4c9ec
yjin-01 <t781797@gmail.com>
2022-07-06 21:43:40 +0900

    Algorithm

--- programmers/programmers-1stages.js
+++ programmers/programmers-1stages.js
@@ -55,3 +55,20 @@ function solution(n) {
   }
   return x;
 }
+
+//약수의 갯수와 덧셈
+
+function solution(left, right) {
+  var answer = 0;
+  for (let i = left; i <= right; i++) {
+    let count = 0;
+    for (let j = 1; j <= i; j++) {
+      if (i % j === 0) {
+        count++;
+      }
+    }
+    count % 2 === 0 ? (answer += i) : (answer -= i);
+  }
+
+  return answer;
+}
