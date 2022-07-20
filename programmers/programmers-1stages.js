1e2e50be69c5822796a0e8aca5d2470d8774502d
yjin-01 <t781797@gmail.com>
2022-07-20 22:49:33 +0900

    Algorithm

--- programmers/programmers-1stages.js
+++ programmers/programmers-1stages.js
@@ -157,3 +157,23 @@ function solution(n, m) {
 
   return answer;
 }
+
+// 완주하지 못한 선수
+
+function solution(participant, completion) {
+  participant.sort(); // 참가자 명단을 오름차순으로 정렬
+  completion.sort((a, b) => (a > b ? 1 : -1)); // 완주자 명단을 오름차순으로 정렬
+
+  let answer = "";
+  for (let i = 0; i < participant.length; i++) {
+    if (participant[i] !== completion[i]) {
+      // 참가자 명단과 완주자 명단을 비교했을 때
+      // 완주자 명단에 없는 참가자를 찾은 후, 바로 반복문을 종료
+      answer = participant[i];
+      break;
+      // return answer;
+    }
+  }
+
+  return answer;
+}
